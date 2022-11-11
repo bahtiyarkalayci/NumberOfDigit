b62d864bf1df2dce87d3e228586ebba4dd59cffa
bahtiyarkalayci <49812375+bahtiyarkalayci@users.noreply.github.com>
2022-11-11 10:49:48 +0300

    https://www.patika.dev/tr

--- src/NumberOfDigit.java
+++ src/NumberOfDigit.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class NumberOfDigit {
+import java.util.Scanner;
+
+public class NumberOfDigit {
+
+    public static void main(String[] args) {
+        //Bir sayının basamak sayılarının toplamını hesaplayan program yazınız.
+        //Örnek : 1643 = 1 + 6 + 4 + 3 = 14
+
+        Scanner scanner=new Scanner(System.in);
+        int number,basamakTop=0;
+        System.out.println("sayıyı giriniz");
+        number=scanner.nextInt();
+        int tempNumber=number;
+        int basnumber;
+
+
+
+        while (tempNumber!=0){
+           basnumber=tempNumber%10;
+            System.out.println("basnumber"+basnumber);
+            basamakTop+=basnumber;
+            tempNumber/=10;
+
+        }
+        System.out.println("basamak sayısı toplamı="+basamakTop);
+    }
 }
